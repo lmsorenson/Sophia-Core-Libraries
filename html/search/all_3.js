c2ec8e66f7744ae2485d5be4376a4418e3cd7eca
lmsorenson <lmsorenson@users.noreply.github.com>
2025-10-02 20:46:28 +0000

    deploy: 1527850f6ce463a84cca01067e53d8f2eb3df20f

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['game_0',['Game',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html',1,'sophia::examples::tic_tac_toe::models']]],
+  ['game_0',['game',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html',1,'sophia::examples::tic_tac_toe::models::Game'],['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html#ad59df6562a58a614fda24622d3715b65',1,'sophia::examples::tic_tac_toe::models::Game::Game()']]],
   ['game_5fover_1',['game_over',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html#a6b830a2c1b1f0b54d410e1481a0faae8',1,'sophia::examples::tic_tac_toe::models::Game']]],
   ['gamestate_2',['GameState',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1GameState.html',1,'sophia::examples::tic_tac_toe::models']]]
 ];
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['game_5fover_0',['game_over',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html#a6b830a2c1b1f0b54d410e1481a0faae8',1,'sophia::examples::tic_tac_toe::models::Game']]]
+  ['game_0',['Game',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html#ad59df6562a58a614fda24622d3715b65',1,'sophia::examples::tic_tac_toe::models::Game']]],
+  ['game_5fover_1',['game_over',['../classsophia_1_1examples_1_1tic__tac__toe_1_1models_1_1Game.html#a6b830a2c1b1f0b54d410e1481a0faae8',1,'sophia::examples::tic_tac_toe::models::Game']]]
 ];
