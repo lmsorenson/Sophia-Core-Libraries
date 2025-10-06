4df4846038c5810c1e605787ec369cbaff96f839
lmsorenson <lsorenson20@gmail.com>
2025-10-06 15:25:08 -0500

    work on documentation for the Tic Tac Toe models.

--- examples/monte_carlo/tic_tac_toe/include/tic_tac_toe/observer/observer.h
+++ examples/monte_carlo/tic_tac_toe/include/tic_tac_toe/observer/observer.h
@@ -4,9 +4,17 @@
 
 namespace sophia::monte_carlo::tic_tac_toe::observer
 {
+    /**
+     * @class Observer
+     * @brief The Observer in an Observer pattern.
+     */
     class Observer
     {
     public:
+        /**
+         * @brief Updates the observer with the given message.
+         * @param message The mssage to update the observer with.
+         */
         virtual void Update(std::string message) = 0;
     };
 }
--- examples/monte_carlo/tic_tac_toe/include/tic_tac_toe/observer/subject.h
+++ examples/monte_carlo/tic_tac_toe/include/tic_tac_toe/observer/subject.h
@@ -6,10 +6,23 @@
 
 namespace sophia::monte_carlo::tic_tac_toe::observer
 {
+    /**
+     * @class Subject
+     * @brief The subject in an observer pattern.
+     */
     class Subject
     {
     public:
+        /**
+         * @brief Adds an observer to the subject.
+         * @param observer The observer to add.
+         */
         void add_observer(const std::shared_ptr<Observer>& observer);
+
+        /**
+         * @brief Notifies the Subject's Observers with a given message.
+         * @param message The message to send to the Observers.
+         */
         void notify(std::string message) const;
 
     private:
